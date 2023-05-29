9b9290daeb0fba7674f00917cbf8f8ee9575f002
Anant <anantsnh@umich.edu>
2023-05-29 01:06:04 -0400

    Use DOI as id

--- run_extra.py
+++ run_extra.py
@@ -34,7 +34,7 @@ class ParquetDataset(Dataset):
 
         row = self.data.iloc[idx]
         content = row[self.type]
-        id = row.id
+        id = row.doi
 
         return {"id": id, "content": content}
 
